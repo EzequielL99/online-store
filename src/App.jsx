3f76876528d3cb261c21ecca70879bd109f2806e
EzequielTecsid <ezequiel@tecsid.com>
2024-09-01 15:22:35 -0300

    Proyecto funcional: carrito persistente

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,176 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Producto from './components/Producto';
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Producto from "./components/Producto";
+import { db } from "./data/db";
 
 function App() {
+  // Initial States
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart');
+
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  }
+
+  // State
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState(initialCart);
+
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+
+  // Effects
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  useEffect(() => {
+    setData(db);
+  }, []);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((product) => product.id === item.id);
+
+    if (itemExists >= 0) {
+      // Existe en el carrito
+      // Validar cantidad del mismo producto ya existente en el carrito
+      if (cart[itemExists].quantity >= MAX_ITEMS) return;
+
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+
+      return item;
+    });
+
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+
+      return item;
+    });
+
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
 
   return (
     <>
-      <Header></Header>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
 
       <main>
-        <section id="promociones" class="section__promociones">
-          <div class="contenedor">
-            <article class="promocion">
-              <div class="promocion__contenido">
-                <p class="promocion__nombre">Oferta de Black Friday</p>
-                <h2 class="promocion__titulo">LAS MEJORES REMERAS</h2>
-                <p class="promocion__descripcion">Nuevo precio: <b>$20000</b></p>
-                <div class="promocion__botones">
-                  <a href="#" class="btn btn__outline-white">Buscar</a>
-                  <a href="#" class="btn btn__outline-white">Comprar</a>
+        <section id="promociones" className="section__promociones">
+          <div className="contenedor">
+            <article className="promocion">
+              <div className="promocion__contenido">
+                <p className="promocion__nombre">Oferta de Black Friday</p>
+                <h2 className="promocion__titulo">LAS MEJORES REMERAS</h2>
+                <p className="promocion__descripcion">
+                  Nuevo precio: <b>$20000</b>
+                </p>
+                <div className="promocion__botones">
+                  <a href="#" className="btn btn__outline-white">
+                    Buscar
+                  </a>
+                  <a href="#" className="btn btn__outline-white">
+                    Comprar
+                  </a>
                 </div>
               </div>
             </article>
-            <article class="promocion">
-              <div class="promocion__contenido">
-                <h2 class="promocion__titulo">Buzos de programador</h2>
-                <p class="promocion__descripcion">
+            <article className="promocion">
+              <div className="promocion__contenido">
+                <h2 className="promocion__titulo">Buzos de programador</h2>
+                <p className="promocion__descripcion">
                   Los mejores buzos con logos de programación
                 </p>
-                <div class="promocion__botones">
-                  <a href="#" class="btn btn__outline-white">Ver</a>
+                <div className="promocion__botones">
+                  <a href="#" className="btn btn__outline-white">
+                    Ver
+                  </a>
                 </div>
               </div>
             </article>
-            <article class="promocion">
-              <div class="promocion__contenido">
-                <h2 class="promocion__titulo">LLegó Angular</h2>
-                <p class="promocion__descripcion">Ya disponible la nueva taza!</p>
-                <div class="promocion__botones">
-                  <a href="#" class="btn btn__outline-white">Comprar</a>
+            <article className="promocion">
+              <div className="promocion__contenido">
+                <h2 className="promocion__titulo">LLegó Angular</h2>
+                <p className="promocion__descripcion">
+                  Ya disponible la nueva taza!
+                </p>
+                <div className="promocion__botones">
+                  <a href="#" className="btn btn__outline-white">
+                    Comprar
+                  </a>
                 </div>
               </div>
             </article>
           </div>
         </section>
 
-        <section id="productos" class="section__productos">
-          <div class="contenedor">
+        <section id="productos" className="section__productos">
+          <div className="contenedor">
             <h2>#TDevShop</h2>
           </div>
 
-          <div class="contenedor productos__grid">
-            <Producto></Producto>
-            <Producto></Producto>
-            <Producto></Producto>
+          <div className="contenedor productos__grid">
+            {data.map((product) => (
+              <Producto
+                key={product.id}
+                product={product}
+                addToCart={addToCart}
+              />
+            ))}
           </div>
         </section>
       </main>
 
-      <footer class="footer">
-        <div class="contenedor">
-          <div class="footer__grid">
-            <div class="footer__empresa">
-              <a href="index.html" class="footer__logo">LOGO</a>
+      <footer className="footer">
+        <div className="contenedor">
+          <div className="footer__grid">
+            <div className="footer__empresa">
+              <a href="index.html" className="footer__logo">
+                LOGO
+              </a>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Provident, iure harum ullam nobis optio expedita. Commodi
                 doloremque quod dolores nesciunt!
               </p>
-              <div class="footer__redes">
+              <div className="footer__redes">
                 <a href="" target="_blank">
                   <i>
                     <svg
@@ -78,15 +180,13 @@ function App() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"
+                      strokeWidth="1.25"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path
-                        d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"
-                      />
+                      <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                     </svg>
                   </i>
                 </a>
@@ -99,10 +199,10 @@ function App() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"
+                      strokeWidth="1.25"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
@@ -119,15 +219,13 @@ function App() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
+                      strokeWidth="1.25"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path
-                        d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"
-                      />
+                      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
                       <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
                       <path d="M16.5 7.5l0 .01" />
                     </svg>
@@ -135,21 +233,21 @@ function App() {
                 </a>
               </div>
             </div>
-            <div class="footer__info">
+            <div className="footer__info">
               <h3>Legales</h3>
               <a href="">Términos de uso</a>
               <a href="">Política de privacidad</a>
               <a href="">Contacto</a>
               <a href="">Newsletter</a>
             </div>
-            <div class="footer__info">
+            <div className="footer__info">
               <h3>Información</h3>
               <a href="">FAQs</a>
               <a href="">Nosotros</a>
               <a href="">Medios de pago</a>
               <a href="">Envíos</a>
             </div>
-            <div class="footer__newsletter">
+            <div className="footer__newsletter">
               <h3>Nuestro newsletter</h3>
               <p>
                 Suscríbete a nuestro newsletter para enterarte las últimas
@@ -157,14 +255,18 @@ function App() {
               </p>
               <form action="#" method="post">
                 <input type="email" placeholder="Escribe tu correo" />
-                <input type="submit" class="btn btn-footer" value="Suscribirse" />
+                <input
+                  type="submit"
+                  className="btn btn-footer"
+                  value="Suscribirse"
+                />
               </form>
             </div>
           </div>
         </div>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,149 @@
+import { useMemo } from "react";
 
-function Header() {
+function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart }) {
+  // State Derivado
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
 
-    return (
-        <header className="header">
-            <div className="contenedor">
-                <div className="flex-logo">
-                    <a href="index.html" className="header__logo">Logo</a>
-                </div>
-                <nav className="navegacion">
-                    <a className="navegacion__enlace navegacion__enlace--activo" href="#"
-                    >Inicio</a
-                    >
-                    <a className="navegacion__enlace" href="nosotros.html">Nosotros</a>
-                </nav>
-                <div className="flex-busqueda">
-                    <a href="" className="header__btn">
-                        <i>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="1.25"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                className="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"
-                            >
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                <path d="M17 17h-11v-14h-2" />
-                                <path d="M6 5l14 1l-1 7h-13" />
-                            </svg>
-                        </i>
-                    </a>
-                    <a href="" className="header__btn">
-                        <i>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="1.25"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                className="icon icon-tabler icons-tabler-outline icon-tabler-search"
-                            >
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                <path d="M21 21l-6 -6" />
-                            </svg>
-                        </i>
-                    </a>
-                </div>
+  return (
+    <header className="header">
+      <div className="contenedor">
+        <div className="flex-logo">
+          <a href="index.html" className="header__logo">
+            Logo
+          </a>
+        </div>
+        <nav className="navegacion">
+          <a className="navegacion__enlace navegacion__enlace--activo" href="#">
+            Inicio
+          </a>
+          <a className="navegacion__enlace" href="nosotros.html">
+            Nosotros
+          </a>
+        </nav>
+        <div className="flex-busqueda">
+          <div className="box__carrito">
+            <a href="" className="header__btn">
+              <i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.25"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                  <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                  <path d="M17 17h-11v-14h-2" />
+                  <path d="M6 5l14 1l-1 7h-13" />
+                </svg>
+              </i>
+            </a>
+            <div id="carrito" className="carrito">
+              {isEmpty ? (
+                <p className="carrito__vacio">El carrito está vacío</p>
+              ) : (
+                <>
+                  <table className="carrito__tabla">
+                    <thead>
+                      <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cart.map(({ id, name, image, price, quantity }) => (
+                        <tr className="item" key={id}>
+                          <td>
+                            <img
+                              src={`./img/${image}.jpg`}
+                              alt="Imagen del producto"
+                            />
+                          </td>
+                          <td>{name}</td>
+                          <td className="item__precio">{price}</td>
+                          <td className="item__cantidad">
+                            <button type="button" onClick={() => decreaseQuantity(id)} className="btn">-</button>
+                            {quantity}
+                            <button type="button" onClick={() => increaseQuantity(id)} className="btn">+</button>
+                            </td>
+                          <td>
+                            <button onClick={() => removeFromCart(id)} className="btn item__btn--eliminar">
+                              <i>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeWidth="1.25"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  className="icon icon-tabler icons-tabler-outline icon-tabler-trash"
+                                >
+                                  <path
+                                    stroke="none"
+                                    d="M0 0h24v24H0z"
+                                    fill="none"
+                                  />
+                                  <path d="M4 7l16 0" />
+                                  <path d="M10 11l0 6" />
+                                  <path d="M14 11l0 6" />
+                                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                </svg>
+                              </i>
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <p className="carrito__total">Total a pagar: ${cartTotal}</p>
+                  <button onClick={clearCart} className="btn btn__limpiar">
+                    Vaciar el carrito
+                  </button>
+                </>
+              )}
             </div>
-        </header>
-    );
+          </div>
+          <a href="" className="header__btn">
+            <i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.25"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                <path d="M21 21l-6 -6" />
+              </svg>
+            </i>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
